0f9889b0edadd5b430ad67a6a60693badd652938
Grant Lin <grant.lin@42-q.com>
2021-03-01 16:49:20 +0800

    Initial commit

---
+++ src/main/java/com/pro/warehouse/StoreManagesysApplication.java
@@ -0,0 +1,13 @@
+package com.pro.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagesysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManagesysApplication.class, args);
+    }
+
+}
